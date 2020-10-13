8fce7cdb042a5219a544692204b8603c45a8b1ab
CundullahT <cundullahterzioglu.it@gmail.com>
2020-10-13 22:51:22 +0200

    Some change on HelloTest.

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,5 +3,8 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("Hello test" );
+
+        System.out.println("Added new change after Necmettin abi asked :d");
+
     }
 }
